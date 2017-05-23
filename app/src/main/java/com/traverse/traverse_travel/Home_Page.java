3b56e371d1e735859058ac2e861b31b2a1c477ac
Daniel Fisher <d22fish@gmail.com>
2017-05-23 16:27:07 -0400

    Home PAge fix

--- app/src/main/java/com/traverse/traverse_travel/Home_Page.java
+++ app/src/main/java/com/traverse/traverse_travel/Home_Page.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+/*i"M A LITTLE TEA CUNT*/
+
 public class Home_Page extends AppCompatActivity {
 
     private static Button planVacationButton;
