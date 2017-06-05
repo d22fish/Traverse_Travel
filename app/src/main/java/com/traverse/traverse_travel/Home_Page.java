dce3faf17afb5938416152459cc5cfb6c6b29680
m201878 <d22fish@gmail.com>
2017-06-04 21:38:35 -0400

    Fixed home page java file to be up to date and with proper links and listeners from clicking buttons.

--- app/src/main/java/com/traverse/traverse_travel/Home_Page.java
+++ app/src/main/java/com/traverse/traverse_travel/Home_Page.java
@@ -8,12 +8,15 @@ import android.widget.Button;
 
 public class Home_Page extends AppCompatActivity {
 
-    private static Button planVacationButton;
     private static Button exploreGlobeButton;
-    private static Button networkingButton;
+    private static Button planVacationButton;
+    private static Button viewCurrentVacationButton;
+    private static Button storedVacationsButton;
+    private static Button networkingAndSocialButton;
     private static Button emergencyCallButton;
     private static Button financesButton;
-    private static Button viewCurrentTripButton;
+    private static Button reportNewEventOrPlaceButton;
+    private static Button settingsButton;
     private static Button miscellaneousButton;
 
 
@@ -21,24 +24,16 @@ public class Home_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        planVacationButtonListener();
         exploreGlobeButtonListener();
-        networkingListener();
-    }
-
-
-    public void planVacationButtonListener() {
-        planVacationButton = (Button) findViewById(R.id.button_planVacation);
-        planVacationButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent("com.example.adrian.traverse_app.PlanVacation");
-                        startActivity(intent);
-
-                    }
-                }
-        );
+        planVacationButtonListener();
+        viewCurrentVacationButtonListener();
+        storedVacationsButtonListener();
+        networkingAndSocialButtonListener();
+        emergencyCallButtonListener();
+        financesButtonListener();
+        reportNewEventOrPlaceButtonListener();
+        settingsButtonListener();
+        miscellaneousButtonListener();
     }
 
     public void exploreGlobeButtonListener() {
@@ -55,14 +50,57 @@ public class Home_Page extends AppCompatActivity {
         );
     }
 
-    public void networkingListener() {
-        networkingButton = (Button) findViewById(R.id.button_networkingAndSocial);
-        networkingButton.setOnClickListener(
+    public void planVacationButtonListener() {
+        planVacationButton = (Button) findViewById(R.id.button_planVacation);
+        planVacationButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("com.traverse.traverse_travel.PlanVacation");
+                        startActivity(intent);
+
+                    }
+                }
+        );
+    }
+
+    public void viewCurrentVacationButtonListener() {
+        viewCurrentVacationButton = (Button) findViewById(R.id.button_viewCurrentVacation);
+        viewCurrentVacationButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
 
-                        Intent intent = new Intent("com.example.adrian.traverse_app.ExploreGlobeActivity");
+                        Intent intent = new Intent("com.traverse.traverse_travel.ViewCurrentVacation");
+                        startActivity(intent);
+
+                    }
+                }
+        );
+    }
+
+    public void storedVacationsButtonListener() {
+        storedVacationsButton = (Button) findViewById(R.id.button_storedVacations);
+        storedVacationsButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("com.traverse.traverse_travel.StoredVacations");
+                        startActivity(intent);
+
+                    }
+                }
+        );
+    }
+
+    public void networkingAndSocialButtonListener() {
+        networkingAndSocialButton = (Button) findViewById(R.id.button_networkingAndSocial);
+        networkingAndSocialButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        Intent intent = new Intent("com.traverse.traverse_travel.NetworkingAndSocial");
                         startActivity(intent);
 
                     }
@@ -76,7 +114,7 @@ public class Home_Page extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Intent intent = new Intent("com.example.adrian.traverse_app.PlanVacation");
+                        Intent intent = new Intent("com.traverse.traverse_travel.EmergencyCall");
                         startActivity(intent);
 
                     }
@@ -84,14 +122,14 @@ public class Home_Page extends AppCompatActivity {
         );
     }
 
-    public void financesListener() {
+    public void financesButtonListener() {
         financesButton = (Button) findViewById(R.id.button_finances);
         financesButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
 
-                        Intent intent = new Intent("com.example.adrian.traverse_app.ExploreGlobeActivity");
+                        Intent intent = new Intent("com.traverse.traverse_travel.finances");
                         startActivity(intent);
 
                     }
@@ -99,13 +137,14 @@ public class Home_Page extends AppCompatActivity {
         );
     }
 
-    public void ViewCurrentTripButtonListener() {
-        viewCurrentTripButton = (Button) findViewById(R.id.button_viewCurrentVacation);
-        viewCurrentTripButton.setOnClickListener(
+    public void reportNewEventOrPlaceButtonListener() {
+        reportNewEventOrPlaceButton = (Button) findViewById(R.id.button_reportNewThing);
+        reportNewEventOrPlaceButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Intent intent = new Intent("com.example.adrian.traverse_app.PlanVacation");
+
+                        Intent intent = new Intent("com.traverse.traverse_travel.ReportNewEventOrPlace");
                         startActivity(intent);
 
                     }
@@ -113,14 +152,29 @@ public class Home_Page extends AppCompatActivity {
         );
     }
 
-    public void miscellaneousListener() {
+    public void settingsButtonListener() {
+        settingsButton = (Button) findViewById(R.id.button_settings);
+        settingsButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        Intent intent = new Intent("com.traverse.traverse_travel.settings");
+                        startActivity(intent);
+
+                    }
+                }
+        );
+    }
+
+    public void miscellaneousButtonListener() {
         miscellaneousButton = (Button) findViewById(R.id.button_miscellaneous);
         miscellaneousButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
 
-                        Intent intent = new Intent("com.example.adrian.traverse_app.ExploreGlobeActivity");
+                        Intent intent = new Intent("com.traverse.traverse_travel.miscellaneous");
                         startActivity(intent);
 
                     }
